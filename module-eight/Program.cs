f82c0fa3e1d877b976c335dad4ffe9b6523e1e96
sahil-naik-kilowott <92841842+sahil-naik-kilowott@users.noreply.github.com>
2021-11-29 16:56:44 +0530

    Add project files.

---
+++ module-eight/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
+
+int letterCount = 0;
+
+foreach (char letter in message)
+{
+    if (letter == 'o')
+    {
+        letterCount++;
+    }
+}
+
+string newMessage = new String(message);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
